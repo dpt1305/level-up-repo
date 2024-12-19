533c9f18017a50ac3ffcdbb71282db3931be063a
dpt1305 <tungplatin@gmail.com>
2024-12-20 06:48:34 +0700

    remove unused imports

--- bt5/src/main/java/org/demo/Main.java
+++ bt5/src/main/java/org/demo/Main.java
@@ -2,7 +2,6 @@ package org.demo;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 
 public class Main {
     public static final Integer MAX_NUMBER = 1000;
--- bt5/src/main/java/org/demo/SumOfArray.java
+++ bt5/src/main/java/org/demo/SumOfArray.java
@@ -1,6 +1,5 @@
 package org.demo;
 
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
