d23052c232d253ec3d7a88d55ce8eeee600ed8bc
tungjj <tungplatin@gmail.com>
2024-12-31 00:00:09 +0700

    add comment description

--- bt6/src/main/java/aden/dev/site/rwlock/MainFunc.java
+++ bt6/src/main/java/aden/dev/site/rwlock/MainFunc.java
@@ -2,9 +2,11 @@ package aden.dev.site.rwlock;
 
 public class MainFunc {
     public static void main(String[] args) {
+        // Unsafe demo
 //        ReadWriteUnsafe rwu = new ReadWriteUnsafe();
 //        rwu.run();
 
+        // Safe demo
         ReadWriteSafe readWriteSafe = new ReadWriteSafe();
         readWriteSafe.run();
     }
