623ca640a6b060b2c3c553f2ff93775101f7b03e
tungjj <tungplatin@gmail.com>
2025-01-10 13:38:31 +0700

    add comments

--- bt8/src/main/java/com/example/bt8/bt8/Bt8Application.java
+++ bt8/src/main/java/com/example/bt8/bt8/Bt8Application.java
@@ -3,6 +3,9 @@ package com.example.bt8.bt8;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+Write test before implements, according to TDD pattern.
+ */
 @SpringBootApplication
 public class Bt8Application {
 
