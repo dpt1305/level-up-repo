c95a412c663edf3167a29520c2530122f946a0de
dpt1305 <tungplatin@gmail.com>
2025-01-13 10:19:14 +0700

    done sub array

--- bt9/src/main/java/com/example/bt9/Main.java
+++ bt9/src/main/java/com/example/bt9/Main.java
@@ -2,11 +2,17 @@ package com.example.bt9;
 
 public class Main {
     public static void main(String[] args) {
-        // Find seond max value 
-        int[] arr = {12, 35, 1, 10, 34, 1};
-        int maxIndex = 1;  // index of 35, which is the maximum value
+        // 1. Find seond max value 
+        // int[] arr = {12, 35, 1, 10, 34, 1};
+        // int maxIndex = 1;  // index of 35, which is the maximum value
         
-        int secondMaxIndex = FindMaxValueInArray.findSecondMaxIndex(arr, maxIndex);
-        System.out.println("Index of second maximum value: " + secondMaxIndex);  // Should print 4 (index of 34)
+        // int secondMaxIndex = FindMaxValueInArray.findSecondMaxIndex(arr, maxIndex);
+        // System.out.println("Index of second maximum value: " + secondMaxIndex);  // Should print 4 (index of 34)
+
+        // 2. Subarray Sum
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int k = 5;
+        int start = 1;
+        SubarraySum.findSubarraySums(arr, k, start);
     }
 }
